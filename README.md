9b8998ca0ad2b57ec7d6534cde88f97d45b676c7
holahu9 <70487513+holahu9@users.noreply.github.com>
2021-03-06 22:08:40 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,16 @@
+# plusOne
+
+Description: A web application created for users looking to meet new friends or dates to attend local events with. Here, users (for example user A) can view local events or create an event of their own and other users who are also interested in the same event as user A, can request to attend the event with user A. When the users sign up to attend the event, they can enter their contact information and then sign up. Then a link to the event will be generated and saved to their profile so they can later view the event details as needed. If user A and another user (for example user B) decide to attend the event together, user A can review both the event and user B after the event. If user B would like to respond to the review, they are also able to do so. The website is open to both users looking for date related activities as well as platonic activities. 
+
+**User Story:** TBD
+
+**Data Flow:** TBD
+
+APIs: TBD
+
+**Task Delegation:**
+1) Ali - Team Lead: SQL, Node, Databases (and some CSS)
+2) Arjun - Wireframe, Routes/Passport and APIs
+3) Duc - Javascript, HTML, CSS and Registration Page
+4) Danielle - APIs and User Testing
+5) Kokhob - HTML, CSS, Handlebars, User Testing
---
+++ app.js
@@ -0,0 +1,75 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const path = require('path');
+//dotenv.config({path:'./.env'});
+const cookiesParser = require('cookie-parser');
+const session = require('express-session');
+const PORT= 4240;
+
+
+
+const db = require("./models"); 
+
+let connection = require('./database/connect');
+
+/*
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DATABASE.PORT,
+})*/ 
+
+app.set('view engine', 'hbs');
+
+const publicDirectory = path.join(__dirname,'./public');
+app.use(express.static(publicDirectory));
+
+
+// Setup cookie
+app.use(session({
+    secret : "plushOne", // Key cookie
+    saveUninitialized: true,
+    resave: true,
+    cookie: { maxAge: 60*60*1000 } // expire time cookie 
+}))
+
+//parse URL-encoded bodies (as sent by HTML forms)
+//make sure we can grab the data from any form
+app.use(express.urlencoded({extended: false}));
+/*
+db.connect((error)=>{
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log("MySQL connected")
+    }
+})
+*/
+
+
+
+
+//grab the form coming in as json
+app.use(express.json());
+app.use(cookiesParser());
+
+//define Routes
+
+app.use('/', require('./routes/pages'));
+app.use('/auth',require('./routes/auth'));
+app.use('/upload',require('./routes/upload'));
+require('./routes/api-routes.js')(app);
+
+
+// Syncing our sequelize models and then starting our Express app
+db.sequelize.sync().then(() => {
+    app.listen(process.env.PORT||PORT, () => {
+        console.log(`Server listening on: http://localhost:${process.env.PORT||PORT}`);
+    })
+})
+  
